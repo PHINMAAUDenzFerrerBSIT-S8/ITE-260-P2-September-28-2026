a082291199c5b035de49cc0c287e30b00c6af293
Ferrer, Denz Gabriel P. <denzferrer8@gmail.com>
2026-09-21 18:21:52 +0800

    Add files via upload

---
+++ ITE 260 P2.py
@@ -0,0 +1,21 @@
+def average(activity1,activity2,activity3):
+    return (activity1 + activity2 + activity3) / 3
+
+Students = int(input("How many students?:"))
+for i in range(Students):
+    Name = input("Enter your name: ")
+    activity1 = int(input("Enter your score in activity 1:"))
+    activity2 = int(input("Enter your score in activity 2:"))
+    activity3 = int(input("Enter your score in activity 3:"))
+    Average = (activity1 + activity2 + activity3) / 3
+
+    print("Name:", Name)
+    print("Average", Average)
+    if Average >= 90:
+        print("Status:Excellent")
+    elif Average >= 80:
+        print("Status:Very Good")
+    elif Average >= 75:
+        print("Status:Passed")
+    else:
+        print("Status:Failed")
